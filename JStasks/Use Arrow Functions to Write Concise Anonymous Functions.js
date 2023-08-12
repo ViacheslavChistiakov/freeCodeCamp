b9b56d76228ad6b0aaad1ebd97b235065eae69a6
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-08-12 18:43:41 +0300

    four task

---
+++ JStasks/Use Arrow Functions to Write Concise Anonymous Functions.js
@@ -0,0 +1,7 @@
+/*DESCRIPTION⚡
+Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also, make sure nothing is defined using the keyword var.
+
+*/
+
+/*SOLUTION🔥 */
+const magic = () => new Date();
