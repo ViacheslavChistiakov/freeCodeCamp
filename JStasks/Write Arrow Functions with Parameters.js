af2df391c4b9014ff870dd8db0660d7d7cd408a9
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-08-12 18:46:00 +0300

    five task

---
+++ JStasks/Write Arrow Functions with Parameters.js
@@ -0,0 +1,9 @@
+/*DESCRIPTION⚡
+Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+*/
+
+/*SOLUTION🔥 */
+const myConcat = (arr1, arr2) =>  arr1.concat(arr2);
+
+
+console.log(myConcat([1, 2], [3, 4, 5]));
