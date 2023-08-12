075a722909ef7beecefcb612f6aadb5f4286d299
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-08-12 18:54:20 +0300

    six task

---
+++ JStasks/Set Default Parameters for Your Functions.js
@@ -0,0 +1,7 @@
+/*DESCRIPTION⚡
+Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+*/
+
+/*SOLUTION🔥 */
+const increment = (number, value = 1) => number + value;
+console.log(increment(0))
