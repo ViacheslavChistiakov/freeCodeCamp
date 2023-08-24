18a89252ff231dda3756cc452854fc6c36220df6
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-08-24 12:04:39 +0300

    twenytwo task

---
+++ JStasks/Create a Module Script.js
@@ -0,0 +1,11 @@
+/*DESCRIPTION⚡
+Add a script to the HTML document of type module and give it the source file of index.js
+
+*/
+
+/*SOLUTION🔥 */
+<html>
+  <body>
+    <script type='module' src='index.js'></script>
+  </body>
+</html>
