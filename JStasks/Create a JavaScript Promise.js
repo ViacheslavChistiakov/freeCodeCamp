4945eec54376d7d8e09c6d8f0ecbcdf4494ad3d8
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-08-31 13:03:32 +0300

    twenyeight task

---
+++ JStasks/Create a JavaScript Promise.js
@@ -0,0 +1,10 @@
+/*DESCRIPTION⚡
+Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+*/
+
+/*SOLUTION🔥 */
+
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
+
