9b4519493751a30f7446e94fd6f8ec20c2e912c4
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-08-31 12:56:31 +0300

    twenysix task

---
+++ JStasks/Create an Export Fallback with export default.js
@@ -0,0 +1,12 @@
+/*DESCRIPTION⚡
+Since export default is used to declare a fallback value for a module or file, you can only have one value be a default export in each module or file. Additionally, you cannot use export default with var, let, or const
+
+The following function should be the fallback value for the module. Please add the necessary code to do so.
+
+*/
+
+/*SOLUTION🔥 */
+
+export default function subtract(x, y) {
+    return x - y;
+  }
