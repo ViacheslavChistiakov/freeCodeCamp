e99eddaeb733ef95e38e8bc87dfaf4d96ece4220
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-04 15:45:43 +0300

    eight  task

---
+++ SASSTasks/Split Your Styles into Smaller Chunks with Partials.jsx
@@ -0,0 +1,12 @@
+/*DESCRIPTION⚡
+Note that the underscore and file extension are not needed in the import statement - Sass understands it is a partial. Once a partial is imported into a file, all variables, mixins, and other code are available to use.
+
+Write an @import statement to import a partial named _variables.scss into the main.scss file.
+
+/*SOLUTION🔥 */
+
+{/* <>
+<!-- The main.scss file -->
+@import 'variables'
+
+</> */}
