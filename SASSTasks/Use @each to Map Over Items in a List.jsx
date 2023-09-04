caf90c612cd7e076bca480415c992380dcdadfbd
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-04 15:25:51 +0300

    six  task

---
+++ SASSTasks/Use @each to Map Over Items in a List.jsx
@@ -0,0 +1,34 @@
+/*DESCRIPTION⚡
+Write an @each directive that goes through a list: blue, black, red and assigns each variable to a .color-bg class, where the color part changes for each item to the respective color. Each class should set the background-color to the respective color as well.
+*/
+
+/*SOLUTION🔥 */
+
+{/* <>
+<style type='text/scss'>
+
+@each $color in blue, black, red {
+  .blue-bg {
+    background-color: blue;
+  }
+
+    .black-bg {
+    background-color: black;
+  }
+
+    .red-bg {
+    background-color: red;
+  }
+}
+
+  div {
+    height: 200px;
+    width: 200px;
+  }
+</style>
+
+<div class="blue-bg"></div>
+<div class="black-bg"></div>
+<div class="red-bg"></div
+
+</> */}
