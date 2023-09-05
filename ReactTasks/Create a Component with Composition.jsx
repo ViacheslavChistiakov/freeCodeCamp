31232d0765e9f5a20ebf6df7c2f4256cc5302079
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-05 14:58:18 +0300

    nine  task

---
+++ ReactTasks/Create a Component with Composition.jsx
@@ -0,0 +1,30 @@
+/*DESCRIPTION⚡
+Note: ChildComponent is defined with an ES6 arrow function because this is a very common practice when using React.
+
+In the code editor, there is a simple functional component called ChildComponent and a class component called ParentComponent. Compose the two together by rendering the ChildComponent within the ParentComponent. Make sure to close the ChildComponent tag with a forward slash.
+/*SOLUTION🔥 */
+
+const ChildComponent = () => {
+    return (
+      <div>
+        <p>I am the child</p>
+      </div>
+    );
+  };
+  
+  class ParentComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>I am the parent</h1>
+          { /* Change code below this line */ }
+            <ChildComponent />
+  
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
