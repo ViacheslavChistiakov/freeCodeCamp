37a84c8a01b44e5f681270a3fce1adafb0a0082f
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-05 14:29:56 +0300

    five  task

---
+++ ReactTasks/Define an HTML Class in JSX.jsx
@@ -0,0 +1,10 @@
+/*DESCRIPTION⚡
+Apply a class of myDiv to the div provided in the JSX code.
+
+/*SOLUTION🔥 */
+
+const JSX = (
+    <div className='myDiv'>
+      <h1>Add a class to this div</h1>
+    </div>
+  );
