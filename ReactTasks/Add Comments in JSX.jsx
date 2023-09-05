9a428c73a50ba61d07b5c61ddd8f56a73138c246
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-05 14:06:14 +0300

    third task

---
+++ ReactTasks/Add Comments in JSX.jsx
@@ -0,0 +1,17 @@
+/*DESCRIPTION⚡
+JSX is a syntax that gets compiled into valid JavaScript. Sometimes, for readability, you might need to add comments to your code. Like most programming languages, JSX has its own way to do this.
+
+
+
+/*SOLUTION🔥 */
+
+
+const JSX = (
+    <div>
+     <h1>This is a block of JSX</h1>
+      <p>
+      {/*Any comments */}
+      Here's a subtitle</p> 
+    </div>
+  );
+     
