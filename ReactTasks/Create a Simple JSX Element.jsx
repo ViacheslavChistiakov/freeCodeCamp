a4653ef434726a2e2d034b43aea17b255068e5c3
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-05 13:52:53 +0300

    first task

---
+++ ReactTasks/Create a Simple JSX Element.jsx
@@ -0,0 +1,5 @@
+/*DESCRIPTION⚡
+The current code uses JSX to assign a div element to the constant JSX. Replace the div with an h1 element and add the text Hello JSX! inside it.
+/*SOLUTION🔥 */
+
+const JSX = <h1>Hello JSX!</h1>;
