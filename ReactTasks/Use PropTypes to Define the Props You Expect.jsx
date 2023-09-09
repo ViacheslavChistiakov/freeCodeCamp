3ad584ca6dfdc2985227f5e4bf8072ef4e7d7986
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-09 16:42:15 +0300

    eighteen task

---
+++ ReactTasks/Use PropTypes to Define the Props You Expect.jsx
@@ -0,0 +1,26 @@
+/*DESCRIPTION⚡
+
+Define propTypes for the Items component to require quantity as a prop and verify that it is of type number.
+
+/*SOLUTION🔥 */
+
+const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  };
+  
+  // Change code below this line
+  Items.propTypes = {quantity: PropTypes.number.isRequired}
+  // Change code above this line
+  
+  Items.defaultProps = {
+    quantity: 0
+  };
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return <Items />
+    }
+  };
