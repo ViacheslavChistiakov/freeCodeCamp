3b1b1b12f1890cfcd312f3439df037ddd5a0f108
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-09 17:24:09 +0300

    twenyone task

---
+++ ReactTasks/Create a Stateful Component.jsx
@@ -0,0 +1,23 @@
+/*DESCRIPTION⚡
+
+There is a component in the code editor that is trying to render a firstName property from its state. However, there is no state defined. Initialize the component with state in the constructor and assign your name to a property of firstName.
+
+/*SOLUTION🔥 */
+
+class StatefulComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      // Only change code below this line
+        this.state = {
+          firstName: 'Slava'
+        }
+      // Only change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.firstName}</h1>
+        </div>
+      );
+    }
+  };
