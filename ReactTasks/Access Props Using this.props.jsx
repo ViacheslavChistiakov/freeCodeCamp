c17ba374ebb8127cadb77d845d30be7bbbd47ef1
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-09 16:52:23 +0300

    nineteen task

---
+++ ReactTasks/Access Props Using this.props.jsx
@@ -0,0 +1,37 @@
+/*DESCRIPTION⚡
+
+Render an instance of the Welcome component in the parent component App. Here, give Welcome a prop of name and assign it a value of a string. Within the child, Welcome, access the name prop within the strong tags.
+
+/*SOLUTION🔥 */
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+              { /* Change code below this line */ }
+              <Welcome name={'Slava'} />
+              { /* Change code above this line */ }
+          </div>
+      );
+    }
+  };
+  
+  class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+            { /* Change code below this line */ }
+            <p>Hello, <strong>{this.props.name}</strong>!</p>
+            { /* Change code above this line */ }
+          </div>
+      );
+    }
+  };
